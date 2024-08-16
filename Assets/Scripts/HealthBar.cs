2725d9fa42d343c9554a0f002845d10d7838f71d
Kunal Shikhar <kunalshikhar2@gmail.com>
2024-08-17 02:24:26 +0530

    bugs fixed

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     Slider _healthSlider;
+    bool isdead = false;
     public SimulateDeath death;
     private void Start()
     {
@@ -13,10 +14,25 @@ public class HealthBar : MonoBehaviour
         _healthSlider.maxValue = 100;
         _healthSlider.value = 100;
     }
+
+    private void Update()
+    {
+        if (isdead)
+        {
+            Target[] asteroids = FindObjectsOfType<Target>();
+            for(int i = 0; i < asteroids.Length; i++)
+            {
+                Destroy(asteroids[i].gameObject);
+            }
+        }
+    }
     public void Damage(float damage)
     {
         _healthSlider.value-=damage;
-        if (_healthSlider.value <=0 )
-        death.Exit();
+        if (_healthSlider.value <= 0)
+        {
+            death.Exit();
+            isdead = true;
+        }
     }
 }
